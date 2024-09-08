3d87c7bb4d9c7d21eded4c26ebfa3b83a4091a6b
el-bryant <bry.y.valen@gmail.com>
2024-09-08 00:10:20 -0500

    cambio de color de fondo de Layout

--- app/src/main/java/code/segael/benefits/ObjetivoActivity.java
+++ app/src/main/java/code/segael/benefits/ObjetivoActivity.java
@@ -15,6 +15,45 @@ public class ObjetivoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityObjetivoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        binding.acvMantener.setOnClickListener(v -> {
+            binding.llayMantener.setBackgroundColor(getResources().getColor(R.color.verde));
+            binding.tvMantenerTitulo.setTextColor(getResources().getColor(R.color.white));
+            binding.tvMantenerContenido.setTextColor(getResources().getColor(R.color.white));
+
+            binding.llaySubir.setBackgroundColor(getResources().getColor(R.color.white));
+            binding.tvSubirTitulo.setTextColor(getResources().getColor(R.color.plomo));
+            binding.tvSubirContenido.setTextColor(getResources().getColor(R.color.plomo));
+
+            binding.llayBajar.setBackgroundColor(getResources().getColor(R.color.white));
+            binding.tvBajarTitulo.setTextColor(getResources().getColor(R.color.plomo));
+            binding.tvBajarContenido.setTextColor(getResources().getColor(R.color.plomo));
+        });
+        binding.acvBajar.setOnClickListener(v -> {
+            binding.llayMantener.setBackgroundColor(getResources().getColor(R.color.white));
+            binding.tvMantenerTitulo.setTextColor(getResources().getColor(R.color.plomo));
+            binding.tvMantenerContenido.setTextColor(getResources().getColor(R.color.plomo));
+
+            binding.llaySubir.setBackgroundColor(getResources().getColor(R.color.white));
+            binding.tvSubirTitulo.setTextColor(getResources().getColor(R.color.plomo));
+            binding.tvSubirContenido.setTextColor(getResources().getColor(R.color.plomo));
+
+            binding.llayBajar.setBackgroundColor(getResources().getColor(R.color.verde));
+            binding.tvBajarTitulo.setTextColor(getResources().getColor(R.color.white));
+            binding.tvBajarContenido.setTextColor(getResources().getColor(R.color.white));
+        });
+        binding.acvSubir.setOnClickListener(v -> {
+            binding.llayMantener.setBackgroundColor(getResources().getColor(R.color.white));
+            binding.tvMantenerTitulo.setTextColor(getResources().getColor(R.color.plomo));
+            binding.tvMantenerContenido.setTextColor(getResources().getColor(R.color.plomo));
+
+            binding.llaySubir.setBackgroundColor(getResources().getColor(R.color.verde));
+            binding.tvSubirTitulo.setTextColor(getResources().getColor(R.color.white));
+            binding.tvSubirContenido.setTextColor(getResources().getColor(R.color.white));
+
+            binding.llayBajar.setBackgroundColor(getResources().getColor(R.color.white));
+            binding.tvBajarTitulo.setTextColor(getResources().getColor(R.color.plomo));
+            binding.tvBajarContenido.setTextColor(getResources().getColor(R.color.plomo));
+        });
         binding.ivVolver.setOnClickListener(v -> {
             startActivity(new Intent(ObjetivoActivity.this, MainActivity.class));
             finish();
